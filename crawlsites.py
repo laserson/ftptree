fc987527620028ed40a3ef2216dc83d111b58177
Uri Laserson <laserson@cloudera.com>
2013-04-15 21:11:10 -0700

    Fixed bugs in crawlsites

--- crawlsites.py
+++ crawlsites.py
@@ -6,17 +6,20 @@ import logging
 from itertools import izip
 from subprocess import Popen
 
-logging.basicConfig(format="%(asctime)s %(message)s")
+logging.basicConfig(format="%(asctime)s %(message)s", level="INFO")
 
 with open('sites.json', 'r') as ip:
     sites = json.load(ip)
 
-cmd = './crawltree.py --host %(host)s --root %(path)s --output %(treefile)s --method %(ftp_list_method)s'
 processes = []
 for site in sites:
+    if site['path'] == '':
+        cmd = './crawltree.py --host %(host)s --output %(treefile)s --method %(ftp_list_method)s'
+    else:
+        cmd = './crawltree.py --host %(host)s --root %(path)s --output %(treefile)s --method %(ftp_list_method)s'
     p = Popen(cmd % site, shell=True)
     processes.append(p)
-    logging.info("INITIATED %s", site['id'])
+    logging.info("INITIATED %s %s", site['id'], cmd % site)
 
 try:
     finished = False
@@ -24,15 +27,16 @@ try:
         logging.info("SLEEPING")
         time.sleep(60)
         returncodes = [p.poll() for p in processes]
-        for (c, p) in izip(returncodes, processes):
+        for (c, s, p) in izip(returncodes, sites, processes):
             if c is None:
-                logging.info("CRAWLING %s", p['id'])
+                logging.info("CRAWLING %s %s", s['id'], p.pid)
             elif c >= 0:
-                logging.info("SUCCESS %s", p['id'])
+                logging.info("SUCCESS %s %s", s['id'], p.pid)
             elif c < 0:
-                logging.info("FAILED %s", p['id'])
+                logging.info("FAILED %s %s", s['id'], p.pid)
         finished = all(returncodes)
 except KeyboardInterrupt:
     logging.info("INTERRUPT")
-    for p in processes:
+    for (s, p) in izip(site, processes):
         p.kill()
+        logging.info("KILLED %s %s", s['id'], p.pid)
